5aa5f93f5caf37e0861afce40521b0d16e396260
a01020809023-glitch <a01020809023@gmail.com>
2025-10-30 15:17:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title('나의 첫 웹 서비스 만들기!')
-st.write('안녕하세요, 감사해용 잘있어용!')
+st.write('안녕하세요, 감사해용 잘있어용! 다시만나용')
 name=st.text_input('너의 이름은!')
 if st.button('인사말 생성'):
   st.write(name+'님!! 반갑습니다!!!')
